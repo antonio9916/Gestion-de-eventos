a1a03d20d49f8024c5a3c5dcbe628a8227d0604c
SUSAN9707 <144391303+SUSAN9707@users.noreply.github.com>
2026-04-24 16:23:06 -0300

    integración final backend/login

--- Backend/App/views.py
+++ Backend/App/views.py
@@ -5,23 +5,40 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import Rol, UsuarioRol
+
+
 @csrf_exempt
 def register_view(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
+
             user = User.objects.create_user(
                 username=data.get('username'),
                 email=data.get('email'),
                 password=data.get('password'),
                 first_name=data.get('name')
             )
+
+            #  ASIGNAR ROL POR DEFECTO (Participante)
+            rol_default = Rol.objects.get(nombre_rol="Participante")
+
+            UsuarioRol.objects.create(
+                usuario=user,
+                rol=rol_default
+            )
+
             return JsonResponse({'message': 'Usuario creado con éxito'}, status=201)
+
         except IntegrityError:
             return JsonResponse({'error': 'Ese usuario o email ya existe'}, status=400)
+
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+
     return JsonResponse({'error': 'Método no permitido'}, status=405)
+
 
 @csrf_exempt
 def login_view(request):
@@ -31,20 +48,27 @@ def login_view(request):
             username = data.get('username')
             password = data.get('password')
 
-            # authenticate verifica las credenciales contra la base de datos
             user = authenticate(username=username, password=password)
 
             if user is not None:
+
+                # OBTENER ROLES DEL USUARIO
+                roles_qs = UsuarioRol.objects.filter(usuario=user).select_related('rol')
+                roles = [r.rol.nombre_rol for r in roles_qs]
+
                 return JsonResponse({
                     'message': 'Inicio de sesión correcto',
-                    'user': {
-                        'username': user.username,
-                        'name': user.first_name
-                    }
+                    'id': user.id,
+                    'username': user.username,
+                    'name': user.first_name,
+                    'email': user.email,
+                    'roles': roles
                 }, status=200)
+
             else:
                 return JsonResponse({'error': 'Usuario o contraseña incorrectos'}, status=400)
+
         except Exception as e:
             return JsonResponse({'error': 'Error en el formato de datos'}, status=400)
-            
+
     return JsonResponse({'error': 'Método no permitido'}, status=405)
